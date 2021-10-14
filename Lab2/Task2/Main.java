7f416d17f146ce8d2dd995aeeacbd02c628d1c8f
IvankaMKS <91254346+IvankaMKS@users.noreply.github.com>
2021-10-14 11:04:47 +0300

    Update Main.java

--- Lab2/Task2/Main.java
+++ Lab2/Task2/Main.java
@@ -1,20 +1,21 @@
 package Lab2.Task2;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        Stack stack = new Stack();
-
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);
-        stack.push(4);
-        stack.push(5);
-
-        for (Node node : stack) {
-            System.out.print(node.getData() + " ");
+    public static void main(String[] args) {
+        final Stack stack = new Stack();
+        stack.add(1);
+        stack.add(2);
+        stack.add(3);
+        stack.add(4);
+        stack.add(5);
+        for (Integer i : stack) {
+            System.out.print(i + " ");
         }
-
-        System.out.println("\n" + "The top element is " + stack.peek());
-        System.out.println("Is stack Empty?  " + stack.isEmpty());
+        System.out.println();
+        System.out.println("After deleting the first item: " );
+        stack.delete();
+        System.out.println(stack);
+        System.out.println("Is stack empty? " + stack.isEmpty());
+        stack.delete();stack.delete();stack.delete();stack.delete();
     }
 }
